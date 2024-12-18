19833b5293dcd3609b680804b86d96e428e183f6
Adam Simon <adamsimonmin@gmail.com>
2024-12-18 16:56:16 +0100

    Day 18 solution

---
+++ 2024/Day 18/Part 1/main.py
@@ -0,0 +1,47 @@
+import heapq
+
+lines = open('Day 18/Part 1/input.txt').read().splitlines()
+
+lines = [line.split(',') for line in lines]
+lines = [[int(x) for x in line] for line in lines]
+
+width = 71
+
+grid = [['.' for _ in range(width)] for _ in range(width)]
+
+for i in range(1024):
+    y, x = lines[i]
+    grid[x][y] = '#'
+
+start = (0, 0)
+end = (width-1, width-1)
+
+pq = []
+heapq.heappush(pq, (0, start[0], start[1]))
+visited = {}
+visited[start] = 0
+
+while pq:
+    cost, x, y = heapq.heappop(pq)
+
+    if (x, y) == end:
+        print(cost)
+        break
+
+    if cost > visited[(x, y)]:
+        continue
+
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nx, ny = x + dx, y + dy
+
+        if not (0 <= nx < width and 0 <= ny < width):
+            continue
+
+        if grid[nx][ny] == '#':
+            continue
+
+        if grid[nx][ny] == '.':
+            new_cost = cost + 1
+            if (nx, ny) not in visited or new_cost < visited[(nx, ny)]:
+                heapq.heappush(pq, (new_cost, nx, ny))
+                visited[(nx, ny)] = new_cost
